375f63ac9774c9b237280d5cb3d8c2fa20f4dbb5
Romain BOBOE <rboboe@gmail.com>
2024-12-13 10:36:13 -0500

    Rollback to old app

--- next.config.js
+++ next.config.js
@@ -14,6 +14,19 @@ const nextConfig = {
     }
     return config;
   },
+  onDemandEntries: {
+    // Period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // Number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
 };
+
+// Handle graceful shutdown
+if (process.env.NODE_ENV === 'development') {
+  process.on('SIGINT', () => {
+    process.exit(0);
+  });
+}
 
 module.exports = nextConfig;
